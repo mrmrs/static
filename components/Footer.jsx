f77f26d7769b814ce7dc68f5045fdec0adf233e0
mrmrs <hi@mrmrs.cc>
2015-10-30 04:03:30 +0800

    LIterally the worst react code

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Footer extends React.Component {
+
+  render() {
+    const styles = {
+      root: {
+      }
+    }
+
+    return (
+      <footer>
+        <p>This is a footer that has not very much content.</p>
+      </footer>
+    )
+  }
+}
+
